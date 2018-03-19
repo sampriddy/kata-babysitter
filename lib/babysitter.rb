d4fcbf2a6b96b13ab827694e9bddedda2846fb75
Sam Priddy <spriddy@covermymeds.com>
2018-03-18 22:19:15 -0400

    some refactors to make it pass

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -3,9 +3,10 @@ class Babysitter
   # in military time. Therefore 17 to 23 are 5:00PM to 11:00PM, and 0 to 4
   # are 12:00AM to 4:00AM on the next day.
   def initialize(start, stop, bed)
-    @start = start
-    @stop = stop
-    @bed = bed
+    @start, @stop, @bed =
+      [start, stop, bed].map do |hour|
+        (0..4).include?(hour) ? hour + 24 : hour
+      end
   end
 
   def calculate_wages
@@ -18,21 +19,23 @@ class Babysitter
 
   private
   def hours_before_bedtime
-    return 0 unless (17...@bed).include?(@start)
+    return 0 unless @start < @bed
 
     hours_difference(@start, [@stop, @bed].min)
   end
 
   def hours_between_bedtime_and_midnight
-    return 0 if (0..4).include?(@start) || (17..@bed).include?(@stop)
+    return 0 if @start >= 24 || @stop <= @bed
 
-    hours_difference([@bed, @start].max, [24, @stop].min)
+    start_time = [@bed, @start].max
+    stop_time  = [24, @stop].min
+
+    hours_difference(start_time, stop_time)
   end
 
   def hours_after_midnight
-    return 0 unless (1..4).include?(@stop)
-
-    hours_difference([0, @start].max, @stop)
+    start_time = [24, @start].max
+    hours_difference(start_time, @stop)
   end
 
   def hours_difference(start, stop)
