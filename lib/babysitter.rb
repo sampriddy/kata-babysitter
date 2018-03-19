6382b31e172d6a6c67b538a64c11f1bd30b88f4f
Sam Priddy <spriddy@covermymeds.com>
2018-03-18 21:50:15 -0400

    Document assumptions around parameters for Babysitter.new

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -1,4 +1,7 @@
 class Babysitter
+  # Start, stop, and bed are all expected to be integers representing hours
+  # in military time. Therefore 17 to 23 are 5:00PM to 11:00PM, and 0 to 4
+  # are 12:00AM to 4:00AM on the next day.
   def initialize(start, stop, bed)
     @start = start
     @stop = stop
